c7afb0b95381949e6d21f94c98ba39a83c96b359
jonas <jonas-scheid@live.de>
2022-01-19 18:47:11 +0100

    fix #184

--- bin/variants2fasta.py
+++ bin/variants2fasta.py
@@ -59,7 +59,7 @@ def read_variant_effect_predictor(file, gene_filter=None):
 
             for co in info.split(","):
                 #Allele|Consequence|IMPACT|SYMBOL|Gene|Feature_type|Feature|BIOTYPE|EXON|INTRON|HGVSc|HGVSp|cDNA_position|CDS_position|Protein_position|Amino_acids|Codons|Existing_variation|DISTANCE|STRAND|FLAGS|SYMBOL_SOURCE|HGNC_ID|TSL|APPRIS|SIFT|PolyPhen|AF|AFR_AF|AMR_AF|EAS_AF|EUR_AF|SAS_AF|AA_AF|EA_AF|gnomAD_AF|gnomAD_AFR_AF|gnomAD_AMR_AF|gnomAD_ASJ_AF|gnomAD_EAS_AF|gnomAD_FIN_AF|gnomAD_NFE_AF|gnomAD_OTH_AF|gnomAD_SAS_AF|CLIN_SIG|SOMATIC|PHENO|PUBMED|MOTIF_NAME|MOTIF_POS|HIGH_INF_POS|MOTIF_SCORE_CHANGE">
-                _,var_type,_,gene,_,transcript_type,transcript_id,_,_,_,_,_,_,transcript_pos,prot_pos,aa_mutation = co.strip().split("|")[:16]
+                _,var_type,_,gene,_,transcript_type,transcript_id,_,_,_,_,_,transcript_pos,_,prot_pos,aa_mutation = co.strip().split("|")[:16]
                 HGNC_ID=co.strip().split("|")[22]
 
                 #pass every other feature type except Transcript (RegulatoryFeature, MotifFeature.)
@@ -73,7 +73,7 @@ def read_variant_effect_predictor(file, gene_filter=None):
 
                     #positioning in Fred2 is 0-based!!!
                     if transcript_pos != "":
-                        coding[transcript_id] = MutationSyntax(transcript_id, int(transcript_pos)-1,
+                        coding[transcript_id] = MutationSyntax(transcript_id, int(transcript_pos.split('/')[0])-1,
                             -1 if prot_pos  == "" else int(prot_pos)-1, co, "", geneID=HGNC_ID)
 
                 #is variant synonymous?
